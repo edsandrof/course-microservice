397a4b558d96a40267223c1a91593a2e02d41a7f
edsandrof <edsandrofreitas@gmail.com>
2020-10-27 19:06:36 -0300

    zuul timeout

--- hr-worker/src/main/java/com/edsandrof/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/edsandrof/hrworker/resources/WorkerResource.java
@@ -31,13 +31,11 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
-        /*
         try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        */
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
         return ResponseEntity.ok().body(workerService.findById(id));
