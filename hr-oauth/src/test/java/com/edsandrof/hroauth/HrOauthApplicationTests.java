951d35808f0c0b50b4aa68766625b63c415cae3b
edsandrof <edsandrofreitas@gmail.com>
2020-10-31 11:40:37 -0300

    project hr-oauth created

---
+++ hr-oauth/src/test/java/com/edsandrof/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsandrof.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
